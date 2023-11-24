0a3cbbaab31395edba45418c9d8984db6b74d9e5
Ben <Lai>
2023-11-24 22:50:08 +1100

    storing ducking value

--- scripts/mods/Speak Up/Speak Up.lua
+++ scripts/mods/Speak Up/Speak Up.lua
@@ -2,8 +2,9 @@
 Title: Speak Up
 Author: Wobin
 Date: 24/04/2023
+Updated: 30/04/2023
 Repository: https://github.com/Wobin/SpeakUp
-Version: 1.0
+Version: 1.1
 ]]--
 
 local mod = get_mod("Speak Up")
@@ -16,23 +17,38 @@ local default_sound_volume = 100
 local currentMusic = 100
 local currentSfx = 100
 
+local musicDucked = false
+local sfxDucked = false
+
 mod.talking = function (self, channel_handle, participant)  
     if participant.is_speaking then
       local duckTo = mod:get("ducking_volume")
       if currentSpeakers[participant.account_id] then return end      
-      Wwise.set_parameter(music_volume_value_name, duckTo)
-      Application.set_user_setting("sound_settings", music_volume_value_name, duckTo)
-      Wwise.set_parameter(sfx_volume_value_name, duckTo)
-      Application.set_user_setting("sound_settings", sfx_volume_value_name, duckTo)
-      Application.save_user_settings()
-      currentSpeakers[participant.account_id] = true
+      if duckTo < currentMusic then
+        Wwise.set_parameter(music_volume_value_name, duckTo)
+        Application.set_user_setting("sound_settings", music_volume_value_name, duckTo)
+        Wwise.set_parameter(sfx_volume_value_name, duckTo)
+        musicDucked = true
+      end
+      if duckTo < currentSfx then
+        Application.set_user_setting("sound_settings", sfx_volume_value_name, duckTo)
+        Application.save_user_settings()
+        currentSpeakers[participant.account_id] = true      
+        sfxDucked = true
+      end
     else
       if currentSpeakers[participant.account_id] and not participant.is_speaking then        
         currentSpeakers[participant.account_id] = nil
-        Wwise.set_parameter(music_volume_value_name, currentMusic)
-        Application.set_user_setting("sound_settings", music_volume_value_name, currentMusic)
-        Wwise.set_parameter(sfx_volume_value_name, currentSfx)
-        Application.set_user_setting("sound_settings", sfx_volume_value_name, currentSfx)
+        if musicDucked then
+          Wwise.set_parameter(music_volume_value_name, currentMusic)
+          Application.set_user_setting("sound_settings", music_volume_value_name, currentMusic)
+          musicDucked = false
+        end
+        if sfxDucked then
+          Wwise.set_parameter(sfx_volume_value_name, currentSfx)
+          Application.set_user_setting("sound_settings", sfx_volume_value_name, currentSfx)
+          sfxDucked = false
+        end
         Application.save_user_settings()
       end
       
